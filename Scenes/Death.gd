f48d14cd013f6fa243052cbb41bdeef294c7da2b
Boadsworth <fat.santa.ce@googlemail.com>
2022-10-01 17:48:48 +0100

    Added death screen

---
+++ Scenes/Death.gd
@@ -0,0 +1,32 @@
+extends Control
+
+func _ready():
+	$CenterContainer/VBoxContainer/VBoxContainer/TextureButton.disabled = true
+	$CenterContainer/VBoxContainer/VBoxContainer.hide()
+	$AnimationPlayer.play("tentacleDeath")
+	yield($AnimationPlayer, "animation_finished")
+	$AnimationPlayer.play("textChange")
+	yield($AnimationPlayer, "animation_finished")
+	$CenterContainer/VBoxContainer/VBoxContainer.show()
+	$CenterContainer/VBoxContainer/HBoxContainer/TextureRect.hide()
+	$AnimationPlayer.play("textIn")
+	yield($AnimationPlayer, "animation_finished")
+	$CenterContainer/VBoxContainer/VBoxContainer/TextureButton.disabled = false
+
+func _on_TextureButton_pressed():
+	var _scene = self.get_tree().change_scene("res://main.tscn")
+
+func _on_TextureButton_mouse_entered():
+	if $CenterContainer/VBoxContainer/VBoxContainer/TextureButton.disabled == false:
+		$menuSelect.play()
+		$CenterContainer/VBoxContainer/VBoxContainer/TextureButton/Label.add_color_override("font_color", Color8(0, 0, 0))
+
+func _on_TextureButton_mouse_exited():
+	if $CenterContainer/VBoxContainer/VBoxContainer/TextureButton.disabled == false:
+		$CenterContainer/VBoxContainer/VBoxContainer/TextureButton/Label.add_color_override("font_color", Color8(255, 255, 255))
+
+func _on_TextureButton_button_down():
+	$CenterContainer/VBoxContainer/VBoxContainer/TextureButton/Label.add_color_override("font_color", Color8(128, 128, 128))
+
+func _on_TextureButton_button_up():
+	$CenterContainer/VBoxContainer/VBoxContainer/TextureButton/Label.add_color_override("font_color", Color8(0, 0, 0))
--- Scenes/you_dead.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _on_TextureButton_pressed():
-	var _scene = self.get_tree().change_scene("res://main.tscn")
