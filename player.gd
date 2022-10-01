9b4b57e3cafaeac01a6bdf9bea90759c8816b9d8
Boadsworth <fat.santa.ce@googlemail.com>
2022-10-01 18:41:20 +0100

    fix

--- player.gd
+++ player.gd
@@ -25,7 +25,7 @@ func checkTile(pos):
 	var cell = tentacleMap.world_to_map(tentacleMap.to_local(pos))
 	var cellv = tentacleMap.get_cellv(cell);
 	if cellv >= 0 and cellv <= 4:
-		var _scene = self.get_tree().change_scene("res://Scenes/you_dead.tscn")
+		var _scene = self.get_tree().change_scene("res://Scenes/Death.tscn")
 	elif cellv == 5:
 		print('NOTE')
 	if cell == Vector2(2, 22):
