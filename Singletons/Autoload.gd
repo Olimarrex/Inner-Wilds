8c16c33b5bfaeba794aa424707a6b18ecfe0acd9
Boadsworth <fat.santa.ce@googlemail.com>
2022-10-02 12:23:18 +0100

    notes JSON

--- Singletons/Autoload.gd
+++ Singletons/Autoload.gd
@@ -2,10 +2,12 @@ extends Node
 
 var victory = ""
 var doorInteractionsDic = {}
+var notesDic = {}
 var doors = []
 
 func _ready():
 	doorInteractionsJSON()
+	notesJSON()
 
 func doorInteractionsJSON():
 	var doorInteractionsFile = File.new()
@@ -13,3 +15,10 @@ func doorInteractionsJSON():
 	var doorInteractionsJSON = JSON.parse(doorInteractionsFile.get_as_text())
 	doorInteractionsFile.close()
 	doorInteractionsDic = doorInteractionsJSON.result
+
+func notesJSON():
+	var notesFile = File.new()
+	notesFile.open("res://Data/notes.json", File.READ)
+	var notesJSON = JSON.parse(notesFile.get_as_text())
+	notesFile.close()
+	notesDic = notesJSON.result
